9ac3da02c30d5cf760204ce2e0aa5047f573fd05
WilsonFilho2 <wilson.menezes09@aluno.ifce.edu.br>
2023-03-02 17:01:00 -0300

    Condicionais

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from. import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:name>', views.greet, name='greet'),
+    path('greet/<str:name>', views.greet, name='greet'),
+    path('tia_do_zap/<str:name>', views.tia_do_zap, name='tia_do_zap'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
@@ -8,5 +9,25 @@ def index(request):
     return render(request, 'index.html')
 
 
-def greet(request, name):
-    return HttpResponse("Olá,"+ name)
+def greet(request, name: str):
+    return render(request, 'greet.html',
+       {'name': name.capitalize()}
+    )
+
+def horas():
+    from datetime import datetime
+
+    agora = datetime.now()
+    hora = agora.strftime("%H")
+
+    if int(hora) <= 18:
+        return True
+    else:
+        return False
+
+def tia_do_zap(request, name: str,):
+    return render(request, 'tia_do_zap.html', 
+        {'name': name.capitalize(),
+         'dia': horas()
+        }
+    )
